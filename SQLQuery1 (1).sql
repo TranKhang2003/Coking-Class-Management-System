6594a9a98ed20059e09cb11c3b67451b9be491a5
lequanghuy <water5ngan@gmai.com>
2024-11-02 11:48:27 +0700

    Update database

--- SQLQuery1 (1).sql
+++ SQLQuery1 (1).sql
@@ -1251,6 +1251,11 @@ begin
 	return @tenquyen;
 end;
 go
+--hàm lấy quyền người dùng hàm chuyển đổi kí tự 
+
+CREATE FUNCTION fn_ConvertToUnsign1 ( @strInput NVARCHAR(4000) ) RETURNS NVARCHAR(4000) AS BEGIN IF @strInput IS NULL RETURN @strInput IF @strInput = '' RETURN @strInput DECLARE @RT NVARCHAR(4000) DECLARE @SIGN_CHARS NCHAR(136) DECLARE @UNSIGN_CHARS NCHAR (136) SET @SIGN_CHARS = N'ăâđêôơưàảãạáằẳẵặắầẩẫậấèẻẽẹéềểễệế ìỉĩịíòỏõọóồổỗộốờởỡợớùủũụúừửữựứỳỷỹỵý ĂÂĐÊÔƠƯÀẢÃẠÁẰẲẴẶẮẦẨẪẬẤÈẺẼẸÉỀỂỄỆẾÌỈĨỊÍ ÒỎÕỌÓỒỔỖỘỐỜỞỠỢỚÙỦŨỤÚỪỬỮỰỨỲỶỸỴÝ' +NCHAR(272)+ NCHAR(208) SET @UNSIGN_CHARS = N'aadeoouaaaaaaaaaaaaaaaeeeeeeeeee iiiiiooooooooooooooouuuuuuuuuuyyyyy AADEOOUAAAAAAAAAAAAAAAEEEEEEEEEEIIIII OOOOOOOOOOOOOOOUUUUUUUUUUYYYYYDD' DECLARE @COUNTER int DECLARE @COUNTER1 int SET @COUNTER = 1 WHILE (@COUNTER <=LEN(@strInput)) BEGIN SET @COUNTER1 = 1 WHILE (@COUNTER1 <=LEN(@SIGN_CHARS)+1) BEGIN IF UNICODE(SUBSTRING(@SIGN_CHARS, @COUNTER1,1)) = UNICODE(SUBSTRING(@strInput,@COUNTER ,1) ) BEGIN IF @COUNTER=1 SET @strInput = SUBSTRING(@UNSIGN_CHARS, @COUNTER1,1) + SUBSTRING(@strInput, @COUNTER+1,LEN(@strInput)-1) ELSE SET @strInput = SUBSTRING(@strInput, 1, @COUNTER-1) +SUBSTRING(@UNSIGN_CHARS, @COUNTER1,1) + SUBSTRING(@strInput, @COUNTER+1,LEN(@strInput)- @COUNTER) BREAK END SET @COUNTER1 = @COUNTER1 +1 END SET @COUNTER = @COUNTER +1 END SET @strInput = replace(@strInput,' ','-') RETURN @strInput END
+GO
+---
 CREATE TRIGGER trg_HoanTienBaoLuu
 ON LopHocNauAn
 AFTER DELETE
@@ -1270,7 +1275,6 @@ BEGIN
     FROM DELETED d
     JOIN LopHocNauAn_HocVien lh ON lh.LopHocNauAnId = d.MaLopHoc;
 END;
----
 go
 CREATE TRIGGER trg_CheckEmail
 ON Users
@@ -1304,3 +1308,10 @@ END;
 
 ----------------------------------------------------------------
 ----------------------------------------------------------------
+--Bổ Sung PROCEDURE
+--tìm kiếm kể cả từ không có dấu 
+create proc P_ViewMonAnByConvert (@str nvarchar(255))
+as
+begin 
+select * from MonAn where dbo.fn_ConvertToUnsign1(MonAn.TenMon) like N'%' + dbo.fn_ConvertToUnsign1(@str) + '%' ;
+end;
